ae03383544ebd3fe3570ec30c7dcc027e7c6ef9d
CGM <76538246+Hokken1012@users.noreply.github.com>
2023-07-08 20:56:43 -0300

    Missing "\*\*.exe"
    First Upload Was Missing "\*\*.exe"

--- AutoShortcut(Run on PowerShell).bat
+++ AutoShortcut(Run on PowerShell).bat
@@ -1,7 +1,7 @@
 $wsh = New-Object -ComObject WScript.Shell
-$items = Get-ChildItem -Path "Origin Path Here"
+$items = Get-ChildItem -Path "(OriginPathHere)\*\*.exe"
 ForEach($item in $items) {
-    $shortcut = $wsh.CreateShortcut("Destination Path Here\$($item.BaseName).lnk")
+    $shortcut = $wsh.CreateShortcut("(DestinationPathHere)\$($item.BaseName).lnk")
     $shortcut.TargetPath = $item.fullname
     $shortcut.WorkingDirectory = $item.FullName -replace [regex]::Escape($item.Name)
 
